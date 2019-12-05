529ad2f59c95f9e3fa965df5196625338909bd34
ANTOINE <38815720+DenisAntoine@users.noreply.github.com>
2019-12-05 14:58:57 +0100

    cosmetique

--- src/main.cpp
+++ src/main.cpp
@@ -12,40 +12,32 @@
 
 /*******************************************************
 / Pinout
-/ TX -> DFPlayer TX via serial (pour economiser pin)
-/ RX -> DFPlayer RX via serial (pour economiser pin)
-/ D1 -> Wire SCL ADS1115 / OledShield / afficheur
-/ D2 -> Wire SDA ADS1115 / OledShield / afficheur
-/ D3 -> OLED_RESET
-/ D4 -> Led Strip
-/ D0 -> 
+/ TX -> 
+/ RX -> 
+/ D1 -> Wire SCL ADS1115 / afficheur 7 segments I2C
+/ D2 -> Wire SDA ADS1115 / afficheur 7 segments I2C
+/ D3 -> 
+/ D4 -> DFPlayer RX via softwareserial
+/ D0 -> Led Strip
 / D5 -> Bouton Rouge / Pull up resistor 10k
 / D6 -> Bouton Bleu / Pull up resistor 10k
-/ D7 -> Software serial RX
-/ D8 -> Software serial TX
+/ D7 -> DFPlayer TX via softwareserial
+/ D8 -> 
 /
 ********************************************************/
-// OLED
-#define OLED_RESET D3  // GPIO0
 
 #define BUTRED_PIN	D5
-#define BUTBLUE_PIN D6
+#define BUTBLUE_PIN 	D6
 #define LED_PIN    	D0
 #define SSRX_PIN   	D4
 #define SSTX_PIN  	D7
 
 
-
-
-
-
 /*******************************************************
- Ledstrip												
+/ Ledstrip												
 *******************************************************/
+#define LED_COUNT 60 // How many NeoPixels are attached to the wemos
 
-// How many NeoPixels are attached to the wemos
-#define LED_COUNT 60
-// Declare our NeoPixel strip object:
 Adafruit_NeoPixel strip(LED_COUNT, LED_PIN, NEO_GRB + NEO_KHZ800);
 // Argument 1 = Number of pixels in NeoPixel strip
 // Argument 2 = Arduino pin number (most are valid)
@@ -57,13 +49,8 @@ Adafruit_NeoPixel strip(LED_COUNT, LED_PIN, NEO_GRB + NEO_KHZ800);
 //   NEO_RGBW    Pixels are wired for RGBW bitstream (NeoPixel RGBW products)
 
 
-/*****************************************************
-OLED Display Variables and constants
-****************************************************/
-//Adafruit_SSD1306 display(OLED_RESET);
-
 // *****************************************************
-// DFPlayer Mini and constants
+// DFPlayer Mini
 // *****************************************************
 SoftwareSerial mySoftwareSerial(SSRX_PIN,SSTX_PIN);
 DFRobotDFPlayerMini myDFPlayer;
@@ -75,18 +62,16 @@ DFRobotDFPlayerMini myDFPlayer;
 Adafruit_ADS1115 ads1115(0x49);
 const uint16_t GOALDETECT = 10000; //sensibilité de detection
 
-
-
 // *****************************************************
 // Equipes, effets sonores et visuels
 // *****************************************************/
 EffetSonore effetson(&myDFPlayer);
 EffetVisuel effetvis(&strip);
-Equipe equipeRouge(2);
-Equipe equipeBleu(1);
+Equipe equipeRouge(1);
+Equipe equipeBleu(2);
 
 /*******************************************************
-/ Boutons												
+/ Definition Boutons												
 / Define the number of devices we have in the chain and
 / the hardware interface
 *******************************************************/
@@ -96,9 +81,13 @@ SmartPins spRed;
 SmartPins spBlue;
 #define DEBOUNCE_TIME 15
 
+/*******************************************************
+/ Methodes declenchees par Smartpins
+/ boutons et encouragements
+********************************************************/
+// boutton rouge
 void redChange(int hilo, int value)
 {
-  // boutton rouge
   if(value > 10000) { // appui de 10s reset des 2 scores
 	  
 	  Serial.println();
@@ -114,14 +103,14 @@ void redChange(int hilo, int value)
 	
   else {
 	  Serial.println();
-	  Serial.println(F("Bouton rouge - plus 1rouge"));
+	  Serial.println(F("Bouton rouge - plus 1 rouge"));
 	  equipeRouge.increaseScore(); // appui court score +1
   }
 }
 
+// boutton bleu
 void blueChange(int hilo, int value)
 {
-  // boutton bleu
   if(value > 10000) { // appui de 10s reset des 2 scores
 	  Serial.println();
 	  Serial.println(F("Bouton bleu >10s - reset scores"));
@@ -139,19 +128,26 @@ void blueChange(int hilo, int value)
 	  equipeBleu.increaseScore(); // appui court score +1
         }
 }
-
+// Encouragements Rouges
 void redcheer()
 {
+	Serial.println();
+	Serial.println(F("Allez les rouges"));
 	equipeRouge.cheer();
 }
+
+// Encouragements Bleus
 void bluecheer()
 {
+	Serial.println();
+	Serial.println(F("Allez les bleus"));
 	equipeBleu.cheer();
 }
 
-// *****************************************************
+/*****************************************************
 // test les buts sur ADS1115
-// *****************************************************/
+// Retourne 0 si pas but, 1 si bleu, 2 si rouge
+*******************************************************/
 
 int testgoal()
 {
@@ -169,11 +165,8 @@ return goal;
 // setup
 // *****************************************************/
 void setup() {
-mySoftwareSerial.begin(9600);
-Serial.begin(115200);
-
-
-
+	mySoftwareSerial.begin(9600);
+	Serial.begin(115200);
 
 // Initialize DFPlayer on softwareserial
 	
@@ -181,60 +174,48 @@ Serial.begin(115200);
 	Serial.println(F("DFRobot DFPlayer Mini Demo"));
 	Serial.println(F("Initializing DFPlayer ... (May take 3~5 seconds)"));
 
-if (!myDFPlayer.begin(mySoftwareSerial)) {  //Utilisation de  softwareSerial pour communiquer
-    Serial.println(F("Pb communication:"));
-    Serial.println(F("1.SVP verifier connexion serie!"));
-    Serial.println(F("2.SVP verifier SDcard !"));
-    while(true);
-  }
-    Serial.println(F("DFPlayer Mini En ligne."));
+	if (!myDFPlayer.begin(mySoftwareSerial)) {  //Utilisation de  softwareSerial pour communiquer
+	    Serial.println(F("Pb communication:"));
+	    Serial.println(F("1.SVP verifier connexion serie!"));
+	    Serial.println(F("2.SVP verifier SDcard !"));
+	    while(true);
+	  }
+    	
+	Serial.println(F("DFPlayer Mini En ligne."));
   
  	myDFPlayer.setTimeOut(500); // Définit un temps de time out sur la communication série à 500 ms
-	myDFPlayer.outputDevice(DFPLAYER_DEVICE_SD);
+	myDFPlayer.outputDevice(DFPLAYER_DEVICE_SD); // lecture sur carte SD
 	myDFPlayer.volume(25);  //Set volume value. From 0 to 30
 	
 	delay(1000);
 	
-
 	Serial.println(F("DFPlayer Mini En ligne."));
 	//effetson.play(1); // joue l intro
 
 
-// initialize Buttons
-spRed.Timed(BUTRED_PIN,INPUT,DEBOUNCE_TIME,redChange);       // external pullup resistor 10k
-spBlue.Timed(BUTBLUE_PIN,INPUT,DEBOUNCE_TIME,blueChange);    // external pullup resistor 10k
-spRed.everyRandom(120000, 580000, redcheer); // encouragements aleatoires
-spBlue.everyRandom(120000, 580000, bluecheer); // encouragements aleatoires
+	// initialize Buttons
+	spRed.Timed(BUTRED_PIN,INPUT,DEBOUNCE_TIME,redChange);       // external pullup resistor 10k
+	spBlue.Timed(BUTBLUE_PIN,INPUT,DEBOUNCE_TIME,blueChange);    // external pullup resistor 10k
+	spRed.everyRandom(120000, 580000, redcheer); // encouragements aleatoires
+	spBlue.everyRandom(120000, 580000, bluecheer); // encouragements aleatoires
 
-// Convertisseur analogiques pour buts
-ads1115.begin();
-Serial.println();
-Serial.println("ADS1115 demarre");
+	// Convertisseur analogiques pour buts
+	ads1115.begin();
+	
+	Serial.println();
+	Serial.println("ADS1115 demarre");
 
-
-/* Initialize LCD DiSplay
-	display.begin(SSD1306_SWITCHCAPVCC, 0x3C);  // initialize with the I2C addr 0x3C (for the 64x48)
-	display.clearDisplay();
-	display.setRotation(2);
-	display.setTextSize(1);
-	display.setTextColor(WHITE);
-	display.setCursor(0,0);
-	display.println("init...");
-	display.display();
-*/
-
-
-
- 
- // Initialize Ledstrip
+	// Initialize Ledstrip
   
 	strip.begin();           // INITIALIZE NeoPixel strip object (REQUIRED)
 	strip.show();            // Turn OFF all pixels ASAP
 	strip.setBrightness(50); // Set BRIGHTNESS to about 1/5 (max = 255)
-	//effetvis.theaterChaseRainbow(10000);
-  Serial.println();
-  Serial.println("strip demarre");
-  // Initialize les equipes
+	
+	Serial.println();
+	Serial.println("strip demarre");
+  	//effetvis.theaterChaseRainbow(10000);
+	
+	// Initialize les equipes
 	equipeRouge.setFolderCheer(1);
 	equipeRouge.setFolderGoal(2);
 	equipeRouge.setFolderWin(3);
@@ -280,33 +261,33 @@ Serial.println("ADS1115 demarre");
 
 void loop() {
   
-  /*
-  Routine pour introduire et modifier la frequence des encouragements
-  spRed.everyRandom(bluermin, bluermax, redcheer); // encouragements aleatoires
-  spBlue.everyRandom(redrmin, redrmax, bluecheer); // encouragements aleatoires
-  */
-  Serial.print(F("Score Rouge :"));
-  Serial.println(equipeRouge.getScore());
-  Serial.print(F("Score Bleu :"));
-  Serial.println(equipeBleu.getScore());
-  
-  switch (testgoal()) {
+ /*
+ Routine pour introduire et modifier la frequence des encouragements
+ spRed.everyRandom(bluermin, bluermax, redcheer); // encouragements aleatoires
+ spBlue.everyRandom(redrmin, redrmax, bluecheer); // encouragements aleatoires
+ */
+ Serial.print(F("Score Rouge :"));
+ Serial.println(equipeRouge.getScore());
+ Serial.print(F("Score Bleu :"));
+ Serial.println(equipeBleu.getScore());
+
+switch (testgoal()) {
   case 1:// but bleu
     Serial.println();
     Serial.println(F("But bleu !"));
     equipeBleu.goal();
-    
   break;
+		
   case 2:// but rouge
     Serial.println();
     Serial.println(F("But Rouge !"))
     equipeRouge.goal();
-
   break;
+		
   default:// pas but
     spRed.loop();// teste les boutons
     spBlue.loop();
-    break;
+  break;
   }
  
 }
